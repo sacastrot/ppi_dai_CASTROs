0eb817c930a89ef08f20d83850e13c83465911f9
Santiago Castro <sacastrot@unal.edu.co>
2024-05-22 14:36:19 -0500

    Setup use environment files for docker container

--- valley_route-b/app/database.py
+++ valley_route-b/app/database.py
@@ -5,14 +5,38 @@ We also define the database URL in the config module and import it here to creat
 """
 
 # Standard library imports
+import os
+from os.path import join, dirname
 
 # Third-party imports
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Local imports (project-specific)
-from config import DATABASE_URL
+# Load environment variables from .env file or environment variables
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+# Get the database connection details from environment variables
+
+# name of the database
+db_name = os.getenv("DATABASE_NAME")
+
+# username for the database
+db_user = os.getenv("DATABASE_USER")
+
+# password for the database
+db_password = os.getenv("DATABASE_PASSWORD")
+
+# host for the database
+db_host = os.getenv("DATABASE_HOST")
+
+# port for the database
+db_port = os.getenv("DATABASE_PORT")
+
+# The database URL is in the format "dialect+driver://username:password@host:port/database"
+DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}/{db_name}"
 
 # Create the SQLAlchemy engine, which is used to connect to the database
 engine = create_engine(DATABASE_URL)
